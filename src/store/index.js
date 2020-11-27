471abba93cb29ac6a3907c8a97cb49ceaee91af9
Yurii Maliarchuk <darksnow91@ukr.net>
2020-11-27 19:03:53 +0200

    update db

--- src/store/index.js
+++ src/store/index.js
@@ -23,7 +23,11 @@ export default new Vuex.Store({
                 description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
                 ingredients: [{
                     ingredientName: "Salt",
-                    ingredientValue: "10mg"
+                    ingredientValue: "20mg"
+                },
+                {
+                    ingredientName: "Pork",
+                    ingredientValue: "1 piece"
                 }]
             },
             {
@@ -33,15 +37,23 @@ export default new Vuex.Store({
                 ingredients: [{
                     ingredientName: "Salt",
                     ingredientValue: "10mg"
+                },
+                {
+                    ingredientName: "Beef",
+                    ingredientValue: "1 piece"
                 }]
             },
             {
-                name: "Vhiken",
+                name: "Chicken",
                 image: "https://image.shutterstock.com/image-photo/grilled-beef-steaks-spices-on-600w-439021402.jpg",
                 description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
                 ingredients: [{
                     ingredientName: "Salt",
-                    ingredientValue: "10mg"
+                    ingredientValue: "15mg"
+                },
+                {
+                    ingredientName: "Chicken",
+                    ingredientValue: "1 piece"
                 }]
             }
         ],
